476d8ed7384ab5b8af39ff9eec7fd3003fb0e234
Geoff Turman <gturman@gmail.com>
2022-12-19 20:35:04 -0500

    cropped templates, bf match tested

--- surftest.py
+++ surftest.py
@@ -1,8 +1,30 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-img = cv.imread('./beer/stella_0.jpg')
-surf = cv.xfeatures2d.SURF_create(10000) #Hessian threshold, best 300-500
-kp, des = surf.detectAndCompute(img, None)
-img2 = cv.drawKeypoints(img, kp, None, (255,0,0) ,4)
-plt.imshow(img2), plt.show()
+HESSIAN_THRESHOLD = 300#200
+FRAME_RESIZE_FACTOR = 2#3
+TEMPLATE_RESIZE_FACTOR = 1#2
+FLANN_DISTANCE_SENS = 0.75#0.75
+FLANN_MATCH_COUNT_SENS = 20 #11
+COMPARE_SENS = 9#9
+
+beerimgpath = './beer/laces_0.jpg'
+beerframeimgpath = './beerframe/hltipa_0.jpg'
+beerimg = cv.imread(beerimgpath)
+beerframeimg = cv.imread(beerframeimgpath)
+
+surf = cv.xfeatures2d.SURF_create(HESSIAN_THRESHOLD)
+kp1, des_beer = surf.detectAndCompute(beerimg,None)
+kp2, des_beerframe = surf.detectAndCompute(beerframeimg,None)
+
+# BFMatcher with default params
+bf = cv.BFMatcher()
+matches = bf.knnMatch(des_beer,des_beerframe,k=2)
+# Apply ratio test
+good = []
+for m,n in matches:
+    if m.distance < 0.75*n.distance:
+        good.append([m])
+# cv.drawMatchesKnn expects list of lists as matches.
+img3 = cv.drawMatchesKnn(beerimg,kp1,beerframeimg,kp2,good,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+plt.imshow(img3),plt.show()
